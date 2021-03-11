f4c8cb01448aaf0986f259531e4cf7644882c8df
Gregor Seegers <gregor.seegers@gmail.com>
2021-03-11 12:11:17 +0100

    intermediate exercise part 1

---
+++ Intermediate/script.js
@@ -0,0 +1,45 @@
+// document.createElement("table").innerHTML += "<table>Language</h3>";
+
+var newdata = JSON.parse(employees);
+
+console.log(newdata);
+
+function tableCreate() {
+  //body reference
+  var body = document.getElementsByTagName("body")[0];
+
+  // create elements <table> and a <tbody>
+  var tbl = document.createElement("table");
+  var tblBody = document.createElement("tbody");
+
+  // cells creation
+  for (var j = 0; j <= 9; j++) {
+    // table row creation
+    var row = document.createElement("tr");
+
+    for (var i = 0; i < 6; i++) {
+      // create element <td> and text node
+      //Make text node the contents of <td> element
+      // put <td> at end of the table row
+      var cell = document.createElement("td");
+      var cellText = document.createTextNode(
+        "cell is row " + j + ", column " + i
+      );
+
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+
+    //row added to end of table body
+    tblBody.appendChild(row);
+  }
+
+  // append the <tbody> inside the <table>
+  tbl.appendChild(tblBody);
+  // put <table> in the <body>
+  body.appendChild(tbl);
+  // tbl border attribute to
+  tbl.setAttribute("border", "2");
+}
+
+tableCreate();
