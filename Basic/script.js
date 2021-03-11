7fef0c9ace2e322ccd30c111ebc48294b32ef5dd
Evelyn Gistelinck <evelyngistelinck@hotmail.com>
2021-03-11 14:24:12 +0100

    solution intermediate ex: table

--- Basic/script.js
+++ Basic/script.js
@@ -2,7 +2,9 @@ var sandwiches = `{
     "sandwich": "hamburger",
     "calories": "260"
 }`;
+// No comma after the last value!!
 var newSandwiches = JSON.parse(sandwiches);
+//JSON.parse can also be used in a JS file that is not a JSON file! to convert the JSON text format.
 console.log(newSandwiches.sandwich);
 
 var fries = ` {
@@ -14,4 +16,5 @@ var newFries = JSON.parse(fries);
 console.log(newFries);
 
 document.getElementById("write").innerHTML = `My favorite sandwich is a ${newSandwiches.sandwich} which has approximately ${newSandwiches.calories} calories, along with it I enjoy eating ${newFries.fries_size} which have about ${newFries.calories} calories.`
+// backtick only needs to be applied once around the whole sentence (not around every $object)
 
--- Intermediate/script.js
+++ Intermediate/script.js
@@ -1,45 +1,42 @@
-// document.createElement("table").innerHTML += "<table>Language</h3>";
-
 var newdata = JSON.parse(employees);
 
 console.log(newdata);
+console.table(newdata);
 
-function tableCreate() {
-  //body reference
-  var body = document.getElementsByTagName("body")[0];
+function createTable() {
+  var table = document.getElementById("myTable");
+  var row = table.insertRow();
+  var cell1 = row.insertCell();
+      var cell2 = row.insertCell();
+      var cell3 = row.insertCell();
+      var cell4 = row.insertCell();
+      var cell5 = row.insertCell();
+      var cell6 = row.insertCell();
+      cell1.innerHTML = "ID";
+      cell2.innerHTML = "First Name";
+      cell3.innerHTML = "Last Name";
+      cell4.innerHTML = "E-mail";
+      cell5.innerHTML = "Job title";
+      cell6.innerHTML = "salary";
 
-  // create elements <table> and a <tbody>
-  var tbl = document.createElement("table");
-  var tblBody = document.createElement("tbody");
-
-  // cells creation
-  for (var j = 0; j <= 9; j++) {
-    // table row creation
-    var row = document.createElement("tr");
-
-    for (var i = 0; i < 6; i++) {
-      // create element <td> and text node
-      //Make text node the contents of <td> element
-      // put <td> at end of the table row
-      var cell = document.createElement("td");
-      var cellText = document.createTextNode(
-        "cell is row " + j + ", column " + i
-      );
-
-      cell.appendChild(cellText);
-      row.appendChild(cell);
-    }
-
-    //row added to end of table body
-    tblBody.appendChild(row);
-  }
-
-  // append the <tbody> inside the <table>
-  tbl.appendChild(tblBody);
-  // put <table> in the <body>
-  body.appendChild(tbl);
-  // tbl border attribute to
-  tbl.setAttribute("border", "2");
+  for (i = 0; i < newdata.length; i++) {
+    // Create an empty <tr> element and add it to the 1st position of the table:
+      var row = table.insertRow();
+    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+      var cell1 = row.insertCell();
+      var cell2 = row.insertCell();
+      var cell3 = row.insertCell();
+      var cell4 = row.insertCell();
+      var cell5 = row.insertCell();
+      var cell6 = row.insertCell(); 
+      
+      // Add some text to the new cells:
+    cell1.innerHTML = newdata[i].ID;
+    cell2.innerHTML = newdata[i].first_name;
+    cell3.innerHTML = newdata[i].last_name;
+    cell4.innerHTML = newdata[i].email;
+    cell5.innerHTML = newdata[i].job_title;
+    cell6.innerHTML = newdata[i].salary;
+  }  
 }
-
-tableCreate();
+createTable();
